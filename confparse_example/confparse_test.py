572a1731a938a5605103e9867174e436f5a8b366
Kirk Byers <ktbyers@twb-tech.com>
2016-11-09 15:31:46 -0500

    Some updates

--- confparse_example/confparse_test.py
+++ confparse_example/confparse_test.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
 
     cisco_cfg = CiscoConfParse(cisco_file)
     intf_obj = cisco_cfg.find_objects(r"^interf")
-
-#    for c_map in crypto_maps:
-#        print
-#        print c_map.text
-#        for child in c_map.children:
-#            print child.text
-#    print
+    for intf in intf_obj:
+        print
+        print intf.text
+        for child in intf.children:
+            print child.text
+        break
+    print
