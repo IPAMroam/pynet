e9f9ef8a78c5e618b709e6945c64fc28c50ac49f
Kirk Byers <ktbyers@twb-tech.com>
2016-11-29 18:36:35 -0800

    Week2 additional exercises

---
+++ followup_exercises/week2/testx.py
@@ -0,0 +1,7 @@
+so what could I do here. I should integrate operational data and executing show commands.
+
+I could configure BGP using Netmiko
+
+I could configure vlans 821, 822, 823 on all four arista switches.
+I could verify that they all exist.
+I could assign arista1, Ethernet7 to be a trunk with vlans 821,822,823
